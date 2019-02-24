ce742b4ba47092a6a35196893f38bcbae76d1ab1
Akash Bera <akashzsky97@gmail.com>
2019-02-24 21:11:32 +0530

    Project Created!

---
+++ SMP_WebApplication/SMP_WebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SMP_WebApplication.Startup))]
+namespace SMP_WebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
